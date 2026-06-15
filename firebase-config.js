c9b2f7cebb43f6c81af11f5a5532b5f6e6bb0cc0
HanBu22 <hanvu26092012@gmail.com>
2026-06-15 18:53:00 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Paste your Firebase web app config here.
 // Firebase Console > Project settings > General > Your apps > Web app.
 export const firebaseConfig = {
-  apiKey: "AIzaSyBnRKitQGBX0u8k4COtDTILYxCJuMf7xzE",
-  authDomain: "exchange-rates-adcf6.firebaseapp.com",
-  projectId: "exchange-rates-adcf6",
-  storageBucket: "exchange-rates-adcf6.firebasestorage.app",
-  messagingSenderId: "875614679042",
-  appId: "1:875614679042:web:5813c3e70a33e91ba0371b",
+  apiKey: "AIzaSyD3B_HRjIGV4PhXwZYSpkExNRwaaIWKplA",
+  authDomain: "globalchat-346a6.firebaseapp.com",
+  projectId: "globalchat-346a6",
+  storageBucket: "globalchat-346a6.firebasestorage.app",
+  messagingSenderId: "277742559072",
+  appId: "1:277742559072:web:0653743d75a5aee6434805",
+  measurementId: "G-K28X6MCVNB",
 };
